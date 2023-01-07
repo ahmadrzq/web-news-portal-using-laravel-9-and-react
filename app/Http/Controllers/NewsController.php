8f19a900a573478d327b2693b66d37090e5c185c
ahmadrzq <ahmadrizqiisnain@gmail.com>
2023-01-08 01:17:11 +0700

    Make card(daisyui) and display data from database

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\NewsCollection;
 use App\Models\News;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
@@ -10,7 +11,7 @@ class NewsController extends Controller
 {
     public function index()
     {
-        $news = News::all();
+        $news = new NewsCollection(News::paginate(20));
         return Inertia::render('Homepage', [
             'title' => "AOVerse",
             'description' => "AOV Universe Has Arrived hahahahaha",
--- resources/js/Pages/Homepage.jsx
+++ resources/js/Pages/Homepage.jsx
@@ -1,24 +1,14 @@
 import { Link, Head } from '@inertiajs/inertia-react';
 import Navbar from '@/Components/Navbar';
+import NewsList from '@/Components/Homepage/NewsList';
 
 export default function Homepage(props) {
-    console.log(props);
     return (
         <div className='min-h-screen bg-indigo-200'>
             <Head title={props.title}></Head>
-            <Navbar/>
-            <div>
-                {props.news ? props.news.map((data,i) =>{
-                    <p>{props.description}</p>
-                    return (
-                        <div key={i} className="p-4 m-2 bg-white text-black shadow-md rounded-md">
-                            <h1 className='text-3xl'>{data.title}</h1>
-                            <p>{data.description}</p>
-                            <h3 className='text-sm'>{data.category}</h3>
-                            <h4 className='text-sm'>{data.author}</h4>
-                        </div>
-                    )
-                }) : <p>No data found</p>}
+            <Navbar />
+            <div className='flex justify-center flex-col lg:flex-row lg:flex-wrap lg:items-stretch items-center gap-4 p-4'>
+                <NewsList news={props.news.data} />
             </div>
         </div>
     );
