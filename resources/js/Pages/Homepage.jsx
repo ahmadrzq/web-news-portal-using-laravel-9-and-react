ecf641cbf95900a37b9264910ff0df3f7f4b77ef
ahmadrzq <ahmadrizqiisnain@gmail.com>
2023-01-08 01:56:49 +0700

    Add Paginator

--- resources/js/Pages/Homepage.jsx
+++ resources/js/Pages/Homepage.jsx
@@ -1,6 +1,7 @@
-import { Link, Head } from '@inertiajs/inertia-react';
+import { Head } from '@inertiajs/inertia-react';
 import Navbar from '@/Components/Navbar';
 import NewsList from '@/Components/Homepage/NewsList';
+import Paginator from '@/Components/Homepage/Paginator';
 
 export default function Homepage(props) {
     return (
@@ -9,6 +10,9 @@ export default function Homepage(props) {
             <Navbar />
             <div className='flex justify-center flex-col lg:flex-row lg:flex-wrap lg:items-stretch items-center gap-4 p-4'>
                 <NewsList news={props.news.data} />
+            </div>
+            <div className='flex justify-center items-center'>
+                <Paginator meta={props.news.meta} />
             </div>
         </div>
     );
