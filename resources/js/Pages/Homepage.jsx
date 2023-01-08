eab72dec86f80bf5cce428485db76c1e92ba9bf8
ahmadrzq <ahmadrizqiisnain@gmail.com>
2023-01-08 08:43:52 +0700

    Adding login and register on navbar menu

--- resources/js/Pages/Homepage.jsx
+++ resources/js/Pages/Homepage.jsx
@@ -7,7 +7,7 @@ export default function Homepage(props) {
     return (
         <div className='min-h-screen bg-indigo-200'>
             <Head title={props.title}></Head>
-            <Navbar />
+            <Navbar user={props.auth.user}/>
             <div className='flex justify-center flex-col lg:flex-row lg:flex-wrap lg:items-stretch items-center gap-4 p-4'>
                 <NewsList news={props.news.data} />
             </div>
